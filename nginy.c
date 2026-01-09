c1d5faf236855f0ee4716c3d9615630fd28d4798
Chi Nguyen <buichinguyen1105@gmail.com>
2026-01-09 15:33:30 +0200

    Add chunking, can send big files now

--- nginy.c
+++ nginy.c
@@ -9,6 +9,20 @@
 #define IP_ADDR INADDR_LOOPBACK
 // INADDR_LOOPBACK is localhost
 
+const char *get_file_type(const char *path) {
+	const char *ext = strrchr(path, '.');
+
+	if (!ext) return "text/plain";
+
+	if (strcmp(ext, ".html") == 0) return "text/html";
+	if (strcmp(ext, ".jpg") == 0) return "image/jpeg";
+	if (strcmp(ext, ".png") == 0) return "image/png";
+	if (strcmp(ext, ".css") == 0) return "text/css";
+	if (strcmp(ext, ".js") == 0) return "application/javascript";
+
+	return "text/plain";
+}
+
 int main() {
 	int fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -48,28 +62,50 @@ int main() {
 		char path[2048] = {0};
 		
 		sscanf(buffer, "%s %s", method, path);
+		printf("Method: %s, Path: %s\n", method, path);
+
+		// Sanitize the path. It only allowed to read inside public/
+		if (strstr(path, "..")) {
+			char *error = "HTTP/1.1 403 Forbidden\n\nAccess Denied!";
+			write(client_fd, error, strlen(error));
+			close(client_fd);
+			continue;
+		}
 
 		if (strcmp(path, "/") == 0) {
 			strcpy(path, "/index.html");
 		}
 
-		FILE *file = fopen(path + 1, "r");
+		char file_path[2048] = {0};
+		sprintf(file_path, "public%s", path);
+
+		FILE *file = fopen(file_path, "r");
 		if (file == NULL) {
 			char *error_response = "HTTP/1.1 404 Not Found\n\nFile Not Found!";
 			write(client_fd, error_response, strlen(error_response));
 		} else {
-			char file_buffer[1024] = {0};
-			int bytes_read = fread(file_buffer, 1, 1024, file);
+			// Get the file size
+			fseek(file, 0, SEEK_END);
+			long fsize = ftell(file);
+			fseek(file, 0, SEEK_SET);
+
+			// Send the header first
+			const char *content_type = get_file_type(file_path);
 			char header[1024] = {0};
 			sprintf(header,
 				"HTTP/1.1 200 OK\n"
-				"Content-Type: text/html\n"
-				"Content-Length: %d\n"
+				"Content-Type: %s\n"
+				"Content-Length: %ld\n"
 				"\n",
-				bytes_read
+				content_type, fsize
 			);
 			write(client_fd, header, strlen(header));
-			write(client_fd, file_buffer, bytes_read);
+
+			char file_buffer[4096] = {0};
+			int bytes_read;
+			while ((bytes_read = fread(file_buffer, 1, 4096, file)) > 0) {
+				write(client_fd, file_buffer, bytes_read);
+			}
 
 			fclose(file);
 		}
